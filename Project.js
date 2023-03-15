cc0acddc43090c439d62ac311e6bca45df89a534
Thrylos13 <127139544+Thrylos13@users.noreply.github.com>
2023-03-16 04:59:24 +0530

    Add files via upload

--- Project.js
+++ Project.js
@@ -1,3 +1,14 @@
+const errorBox = document.getElementById('error-box');
+const errorMessage = document.getElementById('error-message');
+const closeError = document.getElementById('close-error');
+
+function showError(message) {
+  errorMessage.innerText = message;
+  errorBox.classList.add('show');
+}
+function hideError() {
+  errorBox.classList.remove('show');
+}
 let intervalId;
 function displayTime(timezone) {
   clearInterval(intervalId);
@@ -39,33 +50,51 @@ function displayTime(timezone) {
   }, 60000);
 }
 let weather = {
-    apiKey: "939b484a6cf581bc03c41806fb3b486a",
-    fetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data))
-    },
-    displayWeather: function(data) {
-        const { name,timezone } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity, feels_like } = data.main;
-        const { speed } = data.wind;
-        const { lat, lon } = data.coord;
-        console.log(name, icon, description, temp, humidity, speed, lat, lon,timezone)
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".feels_like").innerText = "Feels like: " + feels_like + "°C";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/hr";
-        document.querySelector(".location").innerText = "Latitude: " + lat + "°N" + ", Longitude: " + lon +"°E";
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')";
-        displayTime(data.timezone);  
-    },
-    search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
+  apiKey: "939b484a6cf581bc03c41806fb3b486a",
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      hideError();
+      this.displayWeather(data);
+
+    })
+    .catch((error) => {
+      console.error(error);
+      showError("Error: " + error.message);
+      clearInterval(intervalId);
+    });
+  },
+  displayWeather: function(data) {
+    const { name,timezone } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity, feels_like,pressure } = data.main;
+    const { speed } = data.wind;
+    const { lat, lon } = data.coord;
+    console.log(name, icon, description, temp, humidity,pressure, speed, lat, lon, timezone)
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".feels_like").innerText = "Feels like: " + feels_like + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/hr";
+    document.querySelector(".pressure").innerText = "Pressure: " + pressure + " mb";
+    document.querySelector(".location").innerText = "Latitude: " + lat + "°N" + ", Longitude: " + lon +"°E";
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name +"')";
+    displayTime(data.timezone);
+},
+  search: function () {
+    const searchValue = document.querySelector(".search-bar").value;
+    if (searchValue) {
+      this.fetchWeather(searchValue);
     }
+  },
 };
 document.querySelector(".search button").addEventListener("click", function(){
     weather.search();
